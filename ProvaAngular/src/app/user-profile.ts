ff55bc800462411e998533d4093c9f7705d8d8d6
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2025-02-26 09:47:38 +0100

    Update user-profile.ts

--- ProvaAngular/src/app/user-profile.ts
+++ ProvaAngular/src/app/user-profile.ts
@@ -1,11 +1,19 @@
 import { Component } from "@angular/core";
-
+import {signal, computed} from '@angular/core';
 // user-profile.ts
 @Component({
     selector: 'user-profile',
+    imports: [],
     template: `
       <h1>User profile</h1>
       <p>This is the user profile page</p>
     `,
+    styles: `h1 { background-color: yellow; }`,
   })
-  export class UserProfile { /* Your component code goes here */ }
+  export class UserProfile {
+    firstName = signal('Pinco');
+    lastName = signal('Pallino');
+    fullName = computed(() => `{{ this.firstName() }} {{ this.lastName() }}`)
+  
+    username = signal('p.pallino')
+  }
